00b6e2751ea7fe52064625dbaf4f6e8f2d69934b
hanilizalo23 <nelida_phm@hotmail.com>
2022-10-18 08:58:33 -0500

    UARTFreeRTOS, not working :(

--- examen1_seii.c
+++ examen1_seii.c
@@ -17,6 +17,7 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "event_groups.h"
+#include "freertos_uart.h"
 
 /* TODO: insert other definitions and declarations here. */
 void initial_task(void *pvParameters);
@@ -25,6 +26,7 @@ void second_task(void *pvParameters);
 void third_task(void *pvParameters);
 
 #define NUM_TASKS 	4
+#define FIRST_TASK_P 1
 #define FIRST_TASK_EVENT  1<<0
 #define SECOND_TASK_EVENT 1<<1
 #define SENDER_ONE	1
@@ -39,7 +41,7 @@ typedef uint8_t sender;
 sender sender_number;
 
 typedef struct {
-	const char *msg;
+	const uint8_t *msg;
 } message_t;
 
 int main(void) {
@@ -55,7 +57,7 @@ int main(void) {
 
     PRINTF("Examen 1\n\r ");
 
-    while(!xTaskCreate(initial_task, "initial_task", configMINIMAL_STACK_SIZE+100, NULL, NUM_TASKS, NULL))
+    while(!xTaskCreate(initial_task, "initial_task", configMINIMAL_STACK_SIZE+100, NULL, FIRST_TASK_P, NULL))
     {
     	PRINTF("Failed to create Task \n\r");
     }
@@ -72,11 +74,7 @@ int main(void) {
 
 void initial_task(void *pvParameters)
 {
-	msg_buffer = xQueueCreate(2, sizeof(message_t));
-	msg_event  = xEventGroupCreate();
-	send_msg_mutex = xSemaphoreCreateMutex();
-	take_msg_mutex = xSemaphoreCreateMutex();
-
+	//Tasks creation
 	while(!xTaskCreate(first_task, "send_msg1", configMINIMAL_STACK_SIZE+100, NULL, NUM_TASKS -1, NULL))
 	{
 		PRINTF("Failed to create Task 1 \n\r");
@@ -92,13 +90,29 @@ void initial_task(void *pvParameters)
 		PRINTF("Failed to create Task 3\n\r");
 	}
 
+	//UART configuration
+    freertos_uart_config_t config;
+
+    config.baudrate = 9600;
+    config.rx_pin = 16;
+    config.tx_pin = 17;
+    config.pin_mux = kPORT_MuxAlt3;
+    config.uart_number = freertos_uart0;
+    config.port = freertos_uart_portB;
+    freertos_uart_init(config);
+
+	msg_buffer = xQueueCreate(2, sizeof(message_t));
+	msg_event  = xEventGroupCreate();
+	send_msg_mutex = xSemaphoreCreateMutex();
+	take_msg_mutex = xSemaphoreCreateMutex();
+
 	vTaskSuspend(NULL);
 }
 
 void first_task(void *pvParameters)
 {
 	//Send message
-	const char msg_1[] = "First task\n\r";
+	const uint8_t msg_1[] = "First task\n\r";
 	message_t buffer_msg;
 	buffer_msg.msg = msg_1;
 
@@ -110,12 +124,17 @@ void first_task(void *pvParameters)
 		//Take the mutex to send the message
 		xSemaphoreTake(send_msg_mutex, portMAX_DELAY);
 
+		//Send message
+		freertos_uart_send(freertos_uart0, &buffer_msg.msg, 10);
 		xQueueSend(msg_buffer, &buffer_msg, portMAX_DELAY);
+
 		//Free the mutex to be used by other task
 		xSemaphoreGive(send_msg_mutex);
 
 		//Set the event bit event for this task
 		xEventGroupSetBits(msg_event, FIRST_TASK_EVENT);
+
+		//Blocking
 		vTaskDelay(delay);
 	}
 }
@@ -123,7 +142,7 @@ void first_task(void *pvParameters)
 void second_task(void *pvParameters)
 {
 	//Send message
-	const char msg_2[] = "Second task\n\r";
+	const uint8_t msg_2[] = "Second task\n\r";
 	message_t buffer_msg;
 	buffer_msg.msg = msg_2;
 
@@ -136,12 +155,17 @@ void second_task(void *pvParameters)
 		//Take the mutex to send the message
 		xSemaphoreTake(send_msg_mutex, portMAX_DELAY);
 
+		//Send message
+		freertos_uart_send(freertos_uart0, &buffer_msg.msg, 10);
 		xQueueSend(msg_buffer, &buffer_msg, portMAX_DELAY);
+
 		//Free the mutex to be used by other task
 		xSemaphoreGive(send_msg_mutex);
 
 		//Set the event bit event for this task
 		xEventGroupSetBits(msg_event, SECOND_TASK_EVENT);
+
+		//Blocking
 		vTaskDelay(delay);
 	}
 }
@@ -162,7 +186,7 @@ void third_task(void *pvParameters)
 		xSemaphoreTake(take_msg_mutex,portMAX_DELAY);
 
 		//The message is completed so lets print it
-		PRINTF("%s \n\r", buffer_msg.msg);
+		freertos_uart_receive(freertos_uart0, &buffer_msg.msg, 10);
 
 		//Free the mutex to be used by other task
 		xSemaphoreGive(take_msg_mutex);
